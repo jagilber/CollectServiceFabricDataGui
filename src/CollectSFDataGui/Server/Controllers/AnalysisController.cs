0306454e5a69630480993119f3066e6d500c544b
jason gilbertson <jagilber@microsoft.com>
2023-04-19 22:02:41 -0400

    reverting

---
+++ src/CollectSFDataGui/Server/Controllers/AnalysisController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+
+namespace CollectSFDataGui.Server.Controllers
+{
+    public partial class AnalysisController : Controller
+    {
+
+    }
+}
---
+++ src/CollectSFDataGui/Server/Controllers/CollectionController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Data;
+using System.Collections.Generic;
+//using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
+
+namespace RadzenBlazor
+{
+    public partial class CollectionController : Controller
+    {
+    }
+}
